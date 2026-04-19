00a4ae1f383c722df04bebe9ad3492e5c97ab969
cakilgan <cakilganhan@gmail.com>
2026-04-19 18:14:17 +0300

    change to multiple files one target

--- cmake/lextest.cmake
+++ cmake/lextest.cmake
@@ -1,12 +1,10 @@
 set(_LX_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
-function(lx_add_tests)
-    foreach(source IN LISTS ARGN)
-        get_filename_component(target_name ${source} NAME_WE)
 
-        add_executable(${target_name}
-            ${source}
-            ${_LX_DIR}/entrypoint.cpp
-        )
-        target_link_libraries(${target_name} PRIVATE lextest)
-    endforeach()
+function(lx_add_test target_name)
+    add_executable(${target_name}
+        ${ARGN}
+        ${_LX_DIR}/entrypoint.cpp
+    )
+
+    target_link_libraries(${target_name} PRIVATE lextest)
 endfunction()
