b11d631fb8b18feda8da79df4e6f48b5675cb743
cakilgan <cakilganhan@gmail.com>
2026-04-19 03:06:45 +0300

    fixing unset dir constant

--- cmake/lextest.cmake
+++ cmake/lextest.cmake
@@ -1,10 +1,11 @@
+set(_LX_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
 function(lx_add_tests)
     foreach(source IN LISTS ARGN)
         get_filename_component(target_name ${source} NAME_WE)
 
         add_executable(${target_name}
             ${source}
-            ${LEXTEST_SOURCE_DIR}/entrypoint.cpp
+            ${_LX_DIR}/entrypoint.cpp
         )
         target_link_libraries(${target_name} PRIVATE lextest)
     endforeach()
